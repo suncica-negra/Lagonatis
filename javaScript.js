39213868c31391595c9d3a1d86773ddd9aaef3ce
Dea K <52324941+suncica-negra@users.noreply.github.com>
2020-07-04 14:48:39 +0200

    SEO optimization

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
 	const menuEight = document.querySelector('.menuEight');
-
 	function addClassFunEight() {
 		this.classList.toggle("clickMenuEight");
 	}
-
 	menuEight.addEventListener('click', addClassFunEight);
 });
 
